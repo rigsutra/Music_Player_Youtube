fcf526c0abb9fd3447a240f7f7808d7de27bd7a3
rigsutra <ashishsingh92003@gmail.com>
2025-08-24 10:42:02 +0530

    deployment 1

--- music-app/server/index.js
+++ music-app/server/index.js
@@ -50,7 +50,7 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-// --- Basic in-memory rate limiter (IP based) ---
+// --- Basic in-memory rate limiter (IP based)
 const rateWindowMs = 60_000; // 1 min
 const maxReqPerWindow = 120;
 const ipHits = new Map();
